685018405213902525b213f8dde3d8534522cb64
KavyaDodda <104917434+KavyaDodda@users.noreply.github.com>
2022-05-09 14:07:16 +0530

    Uploaded

---
+++ Week 2 Day 4WebApplication/Controllers/OrderController.cs
@@ -0,0 +1,178 @@
+﻿using BusinessLayer3;
+using DomainLayer3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week_2_Day_4WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderBusiness _orderBusiness;
+
+        public OrderController(ILogger<OrderController> logger, IOrderBusiness orderBusiness)
+        {
+            _logger = logger;
+            _orderBusiness = orderBusiness;
+
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-allOrders")]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetAllOrders();
+
+                if (resp == null || resp.Count == 0)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(OrderDto))]
+        [Route("get-orderById")]
+        public async Task<ActionResult<OrderDto>> GetOrderById(int Id)
+        {
+            try
+            {
+                var resp = await _orderBusiness.GetOrderById(Id);
+
+                if (resp == null)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("insert-order")]
+        public async Task<ActionResult> InsertOrder([FromBody] OrderDto order)
+        {
+            try
+            {
+                await _orderBusiness.InsertOrder(order);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("update-order")]
+        public async Task<ActionResult> UpdateOrder([FromBody] OrderDto order)
+        {
+            try
+            {
+                await _orderBusiness.UpdateOrder(order);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Route("delete-orderById")]
+        public async Task<ActionResult> DeleteOrderById(int Id)
+        {
+            try
+            {
+                await _orderBusiness.DeleteOrderById(Id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-all-orders-by-CreatedTime")]
+        public async Task<ActionResult<List<OrderDto>>> GetAllOrdersByCreatedDate(DateTime dateTime)
+        {
+            try
+            {
+
+                var resp = await _orderBusiness.GetAllOrdersByCreatedDate(dateTime);
+
+                if (resp == null || resp.Count == 0)
+                {
+                    return StatusCode(404, "No Data available.");
+                }
+                return Ok(resp);
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces("application/json", Type = typeof(IEnumerable<OrderDto>))]
+        [Route("get-all-usernames")]
+        public async Task<ActionResult<double>> GetAllUserNames()
+        {
+            try
+            {
+
+                var resp = await _orderBusiness.GetAllUserNames();
+
+                return Ok(resp);
+
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
